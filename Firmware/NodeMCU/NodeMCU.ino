eb5395a3c29a1c36c8d0e36a635a0259bb3e58eb
Amir Aliabadi <amiraliabadi@gmail.com>
2018-04-22 08:52:47 -0700

    code cleanup

--- Firmware/NodeMCU/NodeMCU.ino
+++ Firmware/NodeMCU/NodeMCU.ino
@@ -1,165 +1,55 @@
-#include <Wire.h>
-#include <ESP8266WiFi.h>
-#include <WiFiUdp.h>
- 
-const char* ssid = "aliabadi";
-const char* password = "sunsetsockeye";
- 
-WiFiServer server(80);
+#include <CircularBuffer.h>
 
-WiFiUDP udp;  // A UDP instance to let us send and receive packets over UDP
-unsigned int udpPort = 2311; // local port to listen for UDP packets
-
-// MPU6050 Slave Device Address
-const uint8_t MPU6050SlaveAddress = 0x68;
-
-// Select SDA and SCL pins for I2C communication 
-const uint8_t scl = D6;
-const uint8_t sda = D7;
-
-// sensitivity scale factor respective to full scale setting provided in datasheet 
-const uint16_t AccelScaleFactor = 16384;
-const uint16_t GyroScaleFactor = 131;
-
-// MPU6050 few configuration register addresses
-const uint8_t MPU6050_REGISTER_SMPLRT_DIV   =  0x19;
-const uint8_t MPU6050_REGISTER_USER_CTRL    =  0x6A;
-const uint8_t MPU6050_REGISTER_PWR_MGMT_1   =  0x6B;
-const uint8_t MPU6050_REGISTER_PWR_MGMT_2   =  0x6C;
-const uint8_t MPU6050_REGISTER_CONFIG       =  0x1A;
-const uint8_t MPU6050_REGISTER_GYRO_CONFIG  =  0x1B;
-const uint8_t MPU6050_REGISTER_ACCEL_CONFIG =  0x1C;
-const uint8_t MPU6050_REGISTER_FIFO_EN      =  0x23;
-const uint8_t MPU6050_REGISTER_INT_ENABLE   =  0x38;
-const uint8_t MPU6050_REGISTER_ACCEL_XOUT_H =  0x3B;
-const uint8_t MPU6050_REGISTER_SIGNAL_PATH_RESET  = 0x68;
-
-double Ax, Ay, Az, T, Gx, Gy, Gz;
-int16_t AccelX, AccelY, AccelZ, Temperature, GyroX, GyroY, GyroZ;
+#include "NodeMCU.h"
+#include "MyI2C.h"
+#include "MyWiFi.h"
+#include "MyMPU6050.h"
 
 void setup() {
   Serial.begin(115200);
   delay(10);
+
+  init_WiFi();
   
   Wire.begin(sda, scl);
   MPU6050_Init();
-  
 }
 
-void init_WiFi() {
-  WiFi.begin(ssid, password);
-  
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.println("WiFi connected");
- 
-  // Start the server
-  // server.begin();
-  udp.begin(udpPort);
+CircularBuffer<float,100> gyro_buffer[3];
 
-  Serial.println("Server started");
-  Serial.println(WiFi.localIP());
- 
-  // Print the IP address
-  //Serial.print("Use this URL to connect: ");
-  //Serial.print("http://");
-  //Serial.print(WiFi.localIP());
-  //Serial.println("/");
-}
-
-unsigned long ticks = 0;
-char bffer[1024] ;
 void loop() {
+  t_last = t_now;
+  t_now = millis();
+  
   read_MPU6050();
+  mpu_conversion_process();
 
-  sprintf(bffer,"%i %.3f %.3f %.3f %.1f %.1f %.1f", ticks, Ax, Ay, Az, Gx, Gy, Gz); 
-  Serial.println(bffer);
+  for(int i=0; i<3; i++ ) {
+    gyro_buffer[i].push( i==0?Gx:i==1?Gy:Gz );
+  }
+  float foo[3] = {0.0,0.0,0.0};
 
-  IPAddress broadcastIp = WiFi.localIP();
-  broadcastIp[3] = 255;
-  //Serial.println(broadcastIp);
+  for(int i=0; i<3; i++ ) {
+    for(int j=0; j<gyro_buffer[i].size(); j++ ) {
+      foo[i] += gyro_buffer[i][j];
+    }
+    foo[i] = foo[i]/(gyro_buffer[i].size()*1.0);
+  }
+
+  sprintf(bffer,"%i %.1f %.1f %.1f %.1f %.1f %.1f %.0f %.0f %.0f %.1f %.1f %.1f", 
+    ticks, 
+    Ax, Ay, Az, 
+    Gx, Gy, Gz, 
+    x_ang, y_ang, z_ang, 
+    foo[0], foo[1], foo[2]); 
+  //sprintf(bffer,"%i %.1f %.1f %.1f %.1f %.1f %.1f", ticks, Ax, Ay, Az, Gx, Gy, Gz); 
+  //Serial.println(bffer);
+  
   udp.beginPacket(broadcastIp, udpPort);
-  
-  //udp.print("!AIVDM,1,1,,A,14eG;o@034o8sd<L9i:a;WF>062D,0*7D");
   udp.print(bffer);
-  
   udp.endPacket();
 
   ticks ++;
-  
   delay(10);
 }
 
-void loop_x() {
-  // Check if a client has connected
-  WiFiClient client = server.available();
-  if (!client) {
-    return;
-  }
-
-  read_MPU6050();
-
-  Serial.print("Ax: "); Serial.print(Ax);
-  Serial.print(" Ay: "); Serial.print(Ay);
-  Serial.print(" Az: "); Serial.print(Az);
-  Serial.print(" T: "); Serial.print(T);
-  Serial.print(" Gx: "); Serial.print(Gx);
-  Serial.print(" Gy: "); Serial.print(Gy);
-  Serial.print(" Gz: "); Serial.println(Gz);  
-  
-  delay(1000);
-}
-
-void read_MPU6050() {  
-  Read_RawValue(MPU6050SlaveAddress, MPU6050_REGISTER_ACCEL_XOUT_H);
-  
-  //divide each with their sensitivity scale factor
-  Ax = (double)AccelX/AccelScaleFactor;
-  Ay = (double)AccelY/AccelScaleFactor;
-  Az = (double)AccelZ/AccelScaleFactor;
-  T = (double)Temperature/340+36.53; //temperature formula
-  Gx = (double)GyroX/GyroScaleFactor;
-  Gy = (double)GyroY/GyroScaleFactor;
-  Gz = (double)GyroZ/GyroScaleFactor;
-
-}
-
-void I2C_Write(uint8_t deviceAddress, uint8_t regAddress, uint8_t data){
-  Wire.beginTransmission(deviceAddress);
-  Wire.write(regAddress);
-  Wire.write(data);
-  Wire.endTransmission();
-}
-
-// read all 14 register
-void Read_RawValue(uint8_t deviceAddress, uint8_t regAddress){
-  Wire.beginTransmission(deviceAddress);
-  Wire.write(regAddress);
-  Wire.endTransmission();
-  Wire.requestFrom(deviceAddress, (uint8_t)14);
-  AccelX = (((int16_t)Wire.read()<<8) | Wire.read());
-  AccelY = (((int16_t)Wire.read()<<8) | Wire.read());
-  AccelZ = (((int16_t)Wire.read()<<8) | Wire.read());
-  Temperature = (((int16_t)Wire.read()<<8) | Wire.read());
-  GyroX = (((int16_t)Wire.read()<<8) | Wire.read());
-  GyroY = (((int16_t)Wire.read()<<8) | Wire.read());
-  GyroZ = (((int16_t)Wire.read()<<8) | Wire.read());
-}
-
-//configure MPU6050
-void MPU6050_Init(){
-  delay(150);
-  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_SMPLRT_DIV, 0x07);
-  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_PWR_MGMT_1, 0x01);
-  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_PWR_MGMT_2, 0x00);
-  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_CONFIG, 0x00);
-  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_GYRO_CONFIG, 0x00);//set +/-250 degree/second full scale
-  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_ACCEL_CONFIG, 0x00);// set +/- 2g full scale
-  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_FIFO_EN, 0x00);
-  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_INT_ENABLE, 0x01);
-  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_SIGNAL_PATH_RESET, 0x00);
-  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_USER_CTRL, 0x00);
-}
